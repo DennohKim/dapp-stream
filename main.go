de2faa0c1bd5b3926a00c633d2653b17890c381e
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-10-30 09:57:57 +0300

    packages

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"booking-app/helper"
 )
 
 var conferenceName = "Go Conference"
@@ -21,7 +22,7 @@ func main() {
 
 		firstName, lastName, email, userTickets := getUserInput()
 
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidTicketNumber && isValidName && isValidEmail {
 			bookTickets(userTickets, firstName, lastName, email)
@@ -70,15 +71,6 @@ func getFirstNames() []string {
 	}
 
 	return firstNames
-
-}
-
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) > 2 && len(lastName) > 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isValidTicketNumber
 
 }
 
